f28951f84ad24a52e88b44d0585a98596fa39552
Dimafsfs <123772044+Dimafsfs@users.noreply.github.com>
2023-09-26 15:32:43 +0100

    Add files via upload

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,14 @@
+print("Hi I am Mystical Meredith")
+print("What is your name?")
+input1=input("Enter your name")
+print("Please select a number between 1 and 10")
+input2=input("Enter your number")
+original_number=input2
+result= int(original_number)* 2 *(5)/int(original_number)-7
+print(result)
+
+
+
+
+
+
